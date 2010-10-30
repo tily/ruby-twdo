59e54021a3aba1d447b0328b5b52a5dd5aefdbe3
tily <tily05@gmail.com>
2010-10-30 21:56:17 +0900

    use OAuth::CLI::Twitter.

--- lib/twdo/api.rb
+++ lib/twdo/api.rb
@@ -2,60 +2,29 @@
 require 'rubygems'
 require 'twitter'
 require 'oauth'
+require 'oauth/cli/twitter'
 
 class TwDo
   class API
+    include OAuth::CLI::Twitter
+
     CONF = ENV['HOME'] + '/.twdo'
     CONSUMER_TOKEN  = 'Tok5taGUA8x3VE60w79Q'
     CONSUMER_SECRET = 'qf2UedjS1A9wEqMure0wrq03wWpl4qvAyiCp0dLMd8Q'
 
     def initialize(*args)
+      @access_token = get_access_token(:file => CONF)
       oauth = ::Twitter::OAuth.new(CONSUMER_TOKEN, CONSUMER_SECRET)
-      oauth.authorize_from_access(access[:token], access[:secret])
+      oauth.authorize_from_access(@access_token.token, @access_token.secret)
       @twitter = ::Twitter::Base.new(oauth)
     end
 
     def get
-      @twitter.user('tily').description
+      @twitter.user(@access_token.params[:screen_name]).description
     end
 
     def set(val)
       @twitter.update_profile(:description => val)
-    end
-
-    private
-
-    def access
-      if File.exist?(CONF)
-        YAML::load(File.read(CONF))
-      else
-        authorize_by_oauth
-      end
-    end
-
-    def authorize_by_oauth
-      consumer = OAuth::Consumer.new(
-        CONSUMER_TOKEN,
-        CONSUMER_SECRET,
-        :site => 'http://api.twitter.com',
-        :proxy => ENV['http_proxy']
-      )
-      request_token = consumer.get_request_token
-      puts "Go to URL below and enter pin."
-      puts request_token.authorize_url
-      pin = ''
-      until pin =~ /^\d+$/
-        print '> '
-        pin = STDIN.gets.chomp
-      end
-      begin
-        access_token = request_token.get_access_token(:oauth_verifier => pin)
-      rescue OAuth::Unauthorized => e
-        raise TwDo::Error, 'Invalid pin.'
-      end
-      access = {:token => access_token.token, :secret => access_token.secret}
-      File.open(CONF, 'w') {|f| f.write(access.to_yaml) }
-      access
     end
   end
 end
